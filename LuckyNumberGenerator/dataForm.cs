e1265ec62d865b19236576ea3d52128e2e23b8ac
Noah Vandervelden <NVandervel@my.gcu.edu>
2019-04-11 15:10:00 -0700

    added statement of my own work

--- LuckyNumberGenerator/dataForm.cs
+++ LuckyNumberGenerator/dataForm.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//this is my own work
+//Noah Vandervelden
+
 namespace LuckyNumberGenerator
 {
     public partial class LuckyNumberGenerator : Form
